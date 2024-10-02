a745fc2ed363d40e397ec05d01b9cdd94b3bb29c
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-02 15:08:23 +0900

    fix: 직업 03 이름 수정

--- assets/js/question.js
+++ assets/js/question.js
@@ -36,7 +36,7 @@ function typeText(element, text, speed, callback) {
 const jobs = [
     { name: '쌀 요리사', score: 0 },
     { name: '쌀 유통 전문가', score: 0 },
-    { name: '친환경 농업 연구원', score: 0 },
+    { name: '친환경 쌀 연구원', score: 0 },
     { name: '쌀 농업 경영자', score: 0 },
     { name: '쌀 가공 기술자', score: 0 },
     { name: '쌀 브랜드 마케터', score: 0 },
