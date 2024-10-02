8550eb7331971a6f10fffc31b650289fc8f00d3d
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-02 13:27:46 +0900

    feat: 다시하기 버튼 기능 추가

---
+++ assets/js/result.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const retestButton = document.querySelector(".retest-button");
+  
+    retestButton.addEventListener("click", function() {
+      window.location.href = "index.html"; // index.html로 이동
+    });
+  });
