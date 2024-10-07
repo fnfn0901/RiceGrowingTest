5a29efdb5edc47aff0de4ce36c4609ff57582832
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-07 23:26:51 +0900

    feat: loading.html 아이콘 배치

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // URL에서 쿼리 매개변수를 가져옵니다.
+
+    // URL에서 쿼리 매개변수를 가져오기
     const urlParams = new URLSearchParams(window.location.search);
     const bestJob = urlParams.get('bestJob');
     const worstJob = urlParams.get('worstJob');
 
-    // 5초 후에 result.html로 리다이렉트합니다.
+    // 5초 후에 result.html로 리다이렉트
     setTimeout(function() {
         window.location.href = `result.html?bestJob=${encodeURIComponent(bestJob)}&worstJob=${encodeURIComponent(worstJob)}`;
-    }, 5000); // 5초 대기
+    }, 50000000); // 수정: 5초 대기
 });
